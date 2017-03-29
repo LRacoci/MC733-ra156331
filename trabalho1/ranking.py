ec2db93897aa7d8bbcf857f28483fb8e71888cd2
LRacoci <racoci.0@gmail.com>
2017-03-29 12:58:41 -0300

    Atualisando script em python

--- trabalho1/ranking.py
+++ trabalho1/ranking.py
@@ -14,32 +14,272 @@ except ImportError:
 	flags = None
 
 import numpy as np
-np.set_printoptions(threshold=np.nan)
+np.set_printoptions(threshold=np.nan,linewidth=np.nan, precision=4)
 from numpy import *
 
-def rank(data):
-	data = array(data)
-	indices = array(data[1:, :2], dtype = uint32)
+#import pylab as plt
+
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+from mpl_toolkits.mplot3d import Axes3D
+
+def plotMatrixMap(data, mask, name):
+	data[~mask] = nan
+
+	undef = - nanmax(data)/6
 	
+	data[~mask] = undef
+
+	fig, ax = plt.subplots()
+
+	cax = ax.imshow(data, cmap=plt.cm.rainbow, interpolation='nearest')
+	ax.set_title(name)
+
+	cbar = fig.colorbar(cax, ticks=[undef, 0, 1])
+	cbar.ax.set_yticklabels(['?', '0', '1'])  # vertically oriented colorbar
+
+
+	# Move left and bottom spines outward by 10 points
+	ax.spines['left'].set_position(('outward', 10))
+	ax.spines['bottom'].set_position(('outward', 10))
+	# Hide the right and top spines
+	ax.spines['right'].set_visible(False)
+	ax.spines['top'].set_visible(False)
+	# Only show ticks on the left and bottom spines
+	ax.yaxis.set_ticks_position('left')
+	ax.xaxis.set_ticks_position('bottom')
+
+	ax.set_xlabel('Benchmarks')
+	ax.set_ylabel('Máquinas')
+
+	plt.show()
+
+def plotScater2d12(data, mask, name):
+	nC, nB = mask.shape
+	fig = plt.figure()
+	fig.suptitle(name)
+	axC = fig.add_subplot(121)
+	handles = []
+	labels = []
+	for c in range(nC):
+		bs = (arange(nB)+1)[mask[c,:]]
+		ss = data[c, mask[c,:]]
+		handle, = axC.plot(bs, ss, '.r-', color = cm.rainbow(c/nC), label = "Máquina %d" % (c+1))
+		handles += [handle]
+
+	axC.set_xlabel('Benchmarks')
+	axC.set_ylabel('Scores')
+	axC.set_title("Scores per Benchmarks")
+
+	plt.legend(handles= handles, prop={'size':10})
+
+	axB = fig.add_subplot(122)
+
+	handles = []
+	labels = []
+	for b in range(nB):
+		cs = (arange(nC)+1)[mask[:,b]]
+		ss = data[mask[:,b], b]
+		handle, = axB.plot(cs, ss, '.r-', color = cm.rainbow(b/nB), label = "Benchmark %d" % (b+1))
+		handles += [handle]
+
+	axB.set_xlabel('Máquinas')
+	axB.set_ylabel('Scores')
+	axB.set_title("Scores per Máquinas")
+
+	plt.legend(handles= handles)
+
+	plt.show()
+
+def plotScater2d1(data, mask, name):
+	nC, nB = mask.shape
+	fig, ax = plt.subplots()
+	handles = []
+	labels = []
+	for c in range(nC):
+		bs = (arange(nB)+1)[mask[c,:]]
+		ss = data[c, mask[c,:]]
+		handle, = ax.plot(bs, ss, '.r-', color = cm.rainbow(c/nC), label = "Máquina %d" % (c+1))
+		handles += [handle]
+
+	ax.set_xlabel('Benchmarks')
+	ax.set_ylabel('Scores')
+	ax.set_title(name)
+
+	plt.legend(handles= handles, prop={'size':10})
+
+	plt.show()
+
+def plotScater2d2(data, mask, name):
+	nC, nB = mask.shape
+	fig, ax = plt.subplots()
+	handles = []
+	labels = []
+	for b in range(nB):
+		cs = (arange(nC)+1)[mask[:,b]]
+		ss = data[mask[:,b], b]
+		handle, = ax.plot(cs, ss, '.r-', color = cm.rainbow(b/nB), label = "Benchmark %d" % (b+1))
+		handles += [handle]
+
+	ax.set_xlabel('Máquinas')
+	ax.set_ylabel('Scores')
+	ax.set_title(name)
+
+	plt.legend(handles= handles)
+
+	plt.show()
+
+def plotScater3d(data, mask, name):
+	nC, nB = mask.shape
+
+	fig = plt.figure()
+	ax = fig.add_subplot(111, projection='3d')
+
+	for c in range(nC):
+		cs = full(shape=(nB), fill_value=c+1)[mask[c,:]]
+		bs = (arange(nB)+1)[mask[c,:]]
+		ss = data[c, mask[c,:]]
+		ax.bar(cs, bs, ss, zdir='y', color = cm.rainbow(c/nC))
+
+	ax.set_xlabel('Máquinas')
+	ax.set_ylabel('Benchmarks')
+	ax.set_zlabel('Scores')
+
+	plt.show()
+
+
+def normalize1(data, mask):
+	nC, nB = mask.shape
+	data[~mask] = nan
+	bmax = nanmax(data, axis = 0)
+	bmin = nanmin(data, axis = 0)
+
+	print(bmax)
+	print(bmin)
+	#Initial adjustments
+	data -= kron(bmin, ones((nC, 1)))
+
+	data /= kron((bmax - bmin), ones((nC,1)))
+
+	return data, mask
+
+def normalize2(data, mask):
+	(nC, nB) = mask.shape
+	handled = full(shape = (nB), fill_value = False, dtype = np.bool)
+	print("Começa a normalizar: \n\n")
+	print(data)
+	print(mask)
+	for i in range(0,nC):
+		if(all(handled)):
+			break
+		print(" Handled: ", handled)
+		handle = mask[i,:] & ~handled
+		print("Handling: ", handle)
+		if(any(handle)):
+			data[:,handle] /= kron(data[i,handle], ones((nC, 1)))
+			handled = handled | handle
+			
+
+	return data, mask
+
+	print(data)
+
+def rank(data):
+	arr = array(data[1:])
+	'''
+	struct = [('bench', 'u4'), ('maqui', 'u4'), ('score', 'f8')]
+	mat = empty(arr.shape[0], dtype = struct)
+	mat = rec.array(mat)
+	mat['maqui'] = uint32(arr[:,0])
+	mat['bench'] = uint32(arr[:,1])
+	mat['score'] = float64(arr[:,2])
+	'''
+
+	indices = uint32(arr[:,:2])
+	#print(indices)
 	(nC, nB) = amax(indices, axis = 0)
 
 	scores = empty(shape = (nC, nB), dtype = float64)
 	gotten = full(shape = (nC, nB), fill_value = False, dtype = np.bool)
 	
-	for k in range(1, len(data)):
-		scores[uint32(data[k,0])-1, uint32(data[k,1])-1] = float64(data[k,2])
-		gotten[uint32(data[k,0])-1, uint32(data[k,1])-1] = True
+	for k in range(len(arr)):
+		scores[uint32(arr[k,0])-1, uint32(arr[k,1])-1] = float64(arr[k,2])
+		gotten[uint32(arr[k,0])-1, uint32(arr[k,1])-1] = True
 	
-	print(np.sum(gotten, axis = 1))
+	scores[~gotten] = nan
+	print(scores)
 
+
+	#print(scores[gotten])
+	# Eliminando Benchmarks Problemáticos
 	"""
+	problematics = array([5,6,8,11,12,13])
 	
-	alpha = array(nB, dtype = float64)
+	keep = delete((arange(nB)+1), problematics-1)
+	
+	print("Keep: ", keep)
+	
+	scores = scores[:,keep-1]
+	gotten = gotten[:,keep-1]
+		
+	print(scores)
+	"""
 
-	for comp in scores:
-		for j in range(len(comp)):
+	used = np.sum(gotten, axis = 1)
+	order = rec.array(
+		[used, arange(nC)],
+		dtype = [('used', 'u4'),('comp', 'u4')]
+	)
+	order[::-1].sort(order = 'used')
+	order = order['comp']
+	
+	scores = scores[order,:]
+	gotten = gotten[order,:]
+	
+	print("Tabela Reordenada")
+	print(scores)
+
+	data, mask = normalize2(scores, gotten)
+	
+
+	print(order)
+	usedOrder = order
+	order = rec.array(
+		[order, arange(nC)],
+		dtype = [('old', 'u4'),('new', 'u4')]
+	)
+	
+	order.sort(order = 'old')
+	order = order['new']
+	
+	scores = scores[order,:]
+	gotten = gotten[order,:]
+	plotScater2d1(scores, gotten, "Scores X Benchmarks X Máquinas")
+	plotScater2d2(scores, gotten, "Scores X Máquinas X Benchmarks")
+	plotScater3d(scores, gotten, "Scores X Benchmarks X Máquinas")
+	plotMatrixMap(scores, gotten, "Scores X Benchmarks X Máquinas")
+
+	mean = array([])
 
 
+	
+	"""
+	data, mask = normalize1(scores, gotten)
+	plotMatrixMap(data, mask, "")
+	
+	alpha = empty(gotten.shape, dtype = float64)
+	i = 0
+	j = 1
+	actual = gotten[i]&gotten[j]
+	scores[actual]			
+	print(scores)
+	#print(scores[gotten])
+	for c in range(nC):
+		x = (arange(nC+1)[gotten[c,:]])
+		y = scores[c, gotten[c,:]]
+		plt.scatter(x,y,color = cm.rainbow(c/nC)) 
+
+	plt.show()
 	"""
 
 
@@ -83,7 +323,7 @@ def main():
 	"""Shows basic usage of the Sheets API.
 
 	Creates a Sheets API service object and prints some info:
-	https://docs.google.com/spreadsheets/d/1bHE_ILLOjKkYOd6otzc_2iIkMJ4Uw27h8d58YBZLLO4/edit
+	https://docs.google.com/spreadsheets/d/1P6BALBoYm3LY91y-oUxEYFwfaaxNGRry66Kw0r58huU/edit
 	"""
 	credentials = get_credentials()
 	http = credentials.authorize(httplib2.Http())
@@ -92,8 +332,8 @@ def main():
 	service = discovery.build('sheets', 'v4', http=http,
 							  discoveryServiceUrl=discoveryUrl)
 
-	spreadsheetId = '1bHE_ILLOjKkYOd6otzc_2iIkMJ4Uw27h8d58YBZLLO4'
-	rangeName = 'Medidas Completas!A:D'
+	spreadsheetId = '1P6BALBoYm3LY91y-oUxEYFwfaaxNGRry66Kw0r58huU'
+	rangeName = 'Medidas Completas!A:C'
 	result = service.spreadsheets().values().get(
 		spreadsheetId=spreadsheetId, range=rangeName).execute()
 	values = result.get('values', [])
