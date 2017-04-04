4a80c4819ae61de846628b62141985b5c3c9ff90
Lucas Racoci <racoci.0@gmail.com>
2017-04-04 19:04:08 -0300

    Scripts com políticas

--- exercicio2/scripts.sh
+++ exercicio2/scripts.sh
@@ -23,19 +23,21 @@ do
 
 	this=$ex2/others/max${maxtraces}/${nome[$i]}_${size[$i]}
 
-	for bsize in 4 8 16 32 64 ;
-	do 
-		for assoc in 1 2 4 8 16;
-        do
-		echo "${ex2}/dinero/d4-7/dineroIV -informat s -trname ${nome[$i]}_f2b -maxtrace ${maxtraces} -l1-isize ${size[$i]}k -l1-dsize ${size[$i]}k -l1-ibsize ${bsize} -l1-dbsize ${bsize} -l1-iassoc ${assoc} -l1-dassoc ${assoc} > $ex2/others/${nome[$i]}_${size[$i]}/bsize${bsize}assoc${assoc}"
-
-		$ex2/dinero/d4-7/dineroIV \
-			-informat s -trname ${nome[$i]}_f2b \
-			-maxtrace ${maxtraces} \
-			-l1-isize ${size[$i]}k -l1-dsize ${size[$i]}k \
-			-l1-ibsize ${bsize} -l1-dbsize ${bsize} \
-			-l1-iassoc ${assoc} -l1-dassoc ${assoc} \	
-			> $this/bsize${bsize}assoc${assoc}
+	for bsize in 4 8 16 32 64 ; do 
+		for assoc in 1 2 4 8 16; do
+			for repl in r f;	do
+			echo "$$ex2/dinero/d4-7/dineroIV -informat s -trname ${nome[$i]}_f2b -maxtrace ${maxtraces} -l1-isize ${size[$i]}k -l1-dsize ${size[$i]}k -l1-ibsize ${bsize} -l1-dbsize ${bsize} -l1-iassoc ${assoc} -l1-dassoc ${assoc} -l1-irepl ${repl} -l1-drepl ${repl} > $this/bsize-${bsize}assoc-${assoc}repl-${repl}"
+			
+			$ex2/dinero/d4-7/dineroIV \
+				-informat s -trname ${nome[$i]}_f2b \
+				-maxtrace ${maxtraces} \
+				-l1-isize ${size[$i]}k -l1-dsize ${size[$i]}k \
+				-l1-ibsize ${bsize} -l1-dbsize ${bsize} \
+				-l1-iassoc ${assoc} -l1-dassoc ${assoc} \
+				-l1-irepl ${repl} -l1-drepl ${repl} \
+				> $this/bsize-${bsize}assoc-${assoc}repl-${repl}
+			
+			done
 		done
 	done
 
