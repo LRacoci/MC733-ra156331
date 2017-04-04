91ea56dbca40b8314814edcd768678040a373d10
Lucas Racoci <racoci.0@gmail.com>
2017-04-04 18:53:34 -0300

    Igual PC_IC

--- exercicio2/scripts.sh
+++ exercicio2/scripts.sh
@@ -9,24 +9,33 @@ cd /opt/mc723/benchmarks/traces/
 nome=( 'gcc' 'mesa' 'galgel' 'art' )
 num=( '176' '177' '178' '179' )
 size=( 64 32 8 4)
+traces=/opt/mc723/benchmarks/traces/
+maxtraces=$1
 
 
+cd ${traces}
+
+mkdir $ex2/others/max${maxtraces}
 for((i=0;i<${#nome[@]};i++));
 do
 	cd ${num[$i]}.${nome[$i]}.f2b/
-	mkdir $ex2/others/${nome[$i]}_${size[$i]}
+	mkdir $ex2/others/max${maxtraces}/${nome[$i]}_${size[$i]}
+
+	this=$ex2/others/max${maxtraces}/${nome[$i]}_${size[$i]}
 
 	for bsize in 4 8 16 32 64 ;
 	do 
 		for assoc in 1 2 4 8 16;
-		do
-			$ex2/dinero/d4-7/dineroIV \
-			-informat s -trname "${nome[$i]}_f2b" \
-			-maxtrace 20 \
-			-l1-isize "${size[$i]}k" -l1-dsize "${size[$i]}k" \
+        do
+		echo "${ex2}/dinero/d4-7/dineroIV -informat s -trname ${nome[$i]}_f2b -maxtrace ${maxtraces} -l1-isize ${size[$i]}k -l1-dsize ${size[$i]}k -l1-ibsize ${bsize} -l1-dbsize ${bsize} -l1-iassoc ${assoc} -l1-dassoc ${assoc} > $ex2/others/${nome[$i]}_${size[$i]}/bsize${bsize}assoc${assoc}"
+
+		$ex2/dinero/d4-7/dineroIV \
+			-informat s -trname ${nome[$i]}_f2b \
+			-maxtrace ${maxtraces} \
+			-l1-isize ${size[$i]}k -l1-dsize ${size[$i]}k \
 			-l1-ibsize ${bsize} -l1-dbsize ${bsize} \
-			-l1-iassoc ${assoc} -l1-dassoc ${assoc}
-			> $ex2/others/"${nome[$i]}_${size[$i]}"/"bsize${size[$i]}assoc${assoc}"
+			-l1-iassoc ${assoc} -l1-dassoc ${assoc} \	
+			> $this/bsize${bsize}assoc${assoc}
 		done
 	done
 
