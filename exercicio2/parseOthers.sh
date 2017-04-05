ebe3adbc76dc23f3f044f6c941b466f9ee2696fa
LRacoci <racoci.0@gmail.com>
2017-04-05 01:08:07 -0300

    Plotando 3d

--- exercicio2/parseOthers.sh
+++ exercicio2/parseOthers.sh
@@ -4,36 +4,35 @@ ex2=$(pwd)
 
 #ls /opt/mc723/benchmarks/traces/
 
-cd /opt/mc723/benchmarks/traces/
-
 nome=( 'gcc' 'mesa' 'galgel' 'art' )
-num=( '176' '177' '178' '179' )
 size=( 64 32 8 4)
 traces=/opt/mc723/benchmarks/traces/
 maxtraces=$1
-{parse}=$ex2/{parse}.out
+parse=$ex2/others/parse.out
 
-cd ${traces}
-
-echo "{" >> ${parse}
+echo "d = {" > ${parse}
 for((i=0;i<${#nome[@]};i++));
 do
-	this=$ex2/max${maxtraces}/${nome[$i]}_${size[$i]}
 
-	echo "${nome[i]}:" >> ${parse}
-	
-	for bsize in 4 8 16 32 64 ;
-	do 
-		for assoc in 1 2 4 8 16;
-        do
-		
-		echo "	(($bsize, $assoc), " >> ${parse}
-
-		cat $this/bsize${bsize}assoc${assoc} | grep "Demand miss rate" >> ${parse}
-
-		echo "	)" >> ${parse}
+	this=$ex2/others/max${maxtraces}/${nome[$i]}_${size[$i]}
+	echo "'${nome[$i]}' : np.array([" >>  ${parse}
+	for bsize in 4 8 16 32 64 ; do 
+		echo "[" >>  ${parse}
+		for assoc in 1 2 4 8 16; do
+			echo "[" >>  ${parse}
+			for repl in l r f; do
+			echo "[ #nums" >> ${parse}
+			
+			cat $this/bsize-${bsize}assoc-${assoc}repl-${repl} | grep "Demand miss rate" | sed 's/.*rate\s*\([0-9.]*\).*/\1/' >> ${parse}
+			echo "]," >> ${parse}
+			
+			done
+			echo "]," >>  ${parse}
 		done
+		echo "]," >>  ${parse}
 	done
+
+	echo "], dtype = np.float64)," >>  ${parse}
 
 done
 
