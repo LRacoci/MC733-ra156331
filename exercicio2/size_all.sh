390568be331c0b6be8321520c01f0295acddb0ed
LRacoci <racoci.0@gmail.com>
2017-04-06 10:08:37 -0300

    size_all script

--- exercicio2/size_all.sh
+++ exercicio2/size_all.sh
@@ -9,21 +9,25 @@ cd /home/ubuntu/benchmarks/traces
 num=176
 
 mkdir ${ex2}/size_all
+mkdir ${ex2}/size_all/max$1
+
 for nome in gcc mesa galgel art
 do
 	cd $num.$nome.f2b/
-	mkdir ${nome}
+	this=${ex2}/size_all/max$1/${nome}
+	mkdir ${this}
 
 	for i in 2 4 8 16 32 64 128 256 512;
 	do
 		for d in 2 4 8 16 32 64 128 256 512;
 		do
-			$ex2/dinero/d4-7/dineroIV \
+			echo "dineroIV -informat s -trname ${nome}_f2b -maxtrace $1 -l1-isize ${size}k -l1-dsize $sizek -l1-ibsize 4 -l1-dbsize 4 > $ex2/size_all/${nome}/${i}ki${d}kd.out"
+			dineroIV \
 				-informat s -trname ${nome}_f2b \
 				-maxtrace $1 \
-				-l1-isize $sizek -l1-dsize $sizek \
+				-l1-isize ${i}k -l1-dsize ${d}k \
 				-l1-ibsize 4 -l1-dbsize 4 \
-				> $ex2/size_all/${nome}/${i}ki${d}kd.out
+				> ${this}/${i}ki${d}kd.out
 		done
 	done
 
