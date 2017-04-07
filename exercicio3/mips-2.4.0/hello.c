9828e5aff22c6ebcfb11362a6fd7dd4043dd5765
Lucas Racoci <racoci.0@gmail.com>
2017-04-07 10:58:59 -0300

    Terminando parte prática

--- exercicio3/mips-2.4.0/hello.c
+++ exercicio3/mips-2.4.0/hello.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
 int main(){
-	printf("Hello World !!!!1!!11!!onze\n");
+	int i;
+	int x = 2;
+	int y = 3;
+	printf("Hello World !!!!1!!11!!onze %d\n", x + y);
+	for (i = 0; i < 1000; i++){
+		printf("%d\n", i+x);
+		asm ("add %0, %1, %2": "=r"(x) : "r"(i), "r"(x));
+	}
 	return 0;
 }
 
