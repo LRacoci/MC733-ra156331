bd7a170ce873025f52b9940afba5b68ea88da5cf
LRacoci <racoci.0@gmail.com>
2017-06-15 03:16:22 +0000

    Multiprocs

--- trabalho3/mips-tlm/main.cpp
+++ trabalho3/mips-tlm/main.cpp
@@ -21,53 +21,97 @@ const char *archc_options="-abi -dy ";
 
 #include <stdio.h>
 #include <systemc.h>
+#include <vector>
 #include "mips.H"
 #include "memory.h"
 #include "peripheral.h"
 #include "bus.h"
 
-//#define NUM_PROCS 2
+#define NUM_PROCS 1
+
+using namespace std;
 
 int sc_main(int ac, char *av[])
 {
+#ifdef NUM_PROCS
+	vector< *mips > procs(NUM_PROCS)
+	for(int i = 0; i < NUM_PROCS; i++){
+		char name[] = "mips_______"
+		sprintf(name, "mips%d" i+1);
+		procs[i] = new mips(name);
+	}
+#else
+	//!  ISA simulator
+	mips mips_proc1("mips1");
+#endif
 
-  //!  ISA simulator
-  mips mips_proc1("mips1");
-
-  //! Bus
-  ac_tlm_bus bus("bus");
-  // Memory
-  ac_tlm_mem mem("mem");
-  // Peripheral
-  ac_tlm_peripheral peripheral("peripheral");
+	//! Bus
+	ac_tlm_bus bus("bus");
+	// Memory
+	ac_tlm_mem mem("mem");
+	// Peripheral
+	ac_tlm_peripheral peripheral("peripheral");
 
 #ifdef AC_DEBUG
 
-  ac_trace("mips1_proc1.trace");
-  
+	ac_trace("mips1_proc1.trace");
+	
 #endif
-  
-  mips_proc1.DM(bus.target_export);
-  bus.MEM_port(mem.target_export);
-  bus.PERIPHERAL_port(peripheral.target_export);
 
-  mips_proc1.init(ac, av);
-  mips_proc1.set_prog_args();
-  cerr << endl;
-  
-  sc_start();
+#ifdef NUM_PROCS
+	for(int i = 0; i < NUM_PROCS; i++){
+		procs[i]->DM(bus.target_export);
+	}
+#else
+	mips_proc1.DM(bus.target_export);
+#endif
+	bus.MEM_port(mem.target_export);
+	bus.PERIPHERAL_port(peripheral.target_export);
 
-  mips_proc1.PrintStat();
-  cerr << endl;
+#ifdef NUM_PROCS
+	for(int i = 0; i < NUM_PROCS; i++){
+		char** avi = (char**) malloc(ac*sizeof(char**));
+		memcpy (avi, av, ac*sizeof(char**));
+		procs[i]->init(ac, avi);
+		procs[i]->set_prog_args();
+	}
+#else
+	mips_proc1.init(ac, av);
+	mips_proc1.set_prog_args();
+#endif
+
+	cerr << endl;
+	
+	sc_start();
+
+#ifdef NUM_PROCS
+	for(int i = 0; i < NUM_PROCS; i++){
+		procs[i]->PrintStat();
+	}
+#else
+	mips_proc1.PrintStat();
+#endif
+
+	cerr << endl;
 
 #ifdef AC_STATS
-  mips1_proc1.ac_sim_stats.time = sc_simulation_time();
-  mips1_proc1.ac_sim_stats.print();
+#ifdef NUM_PROCS
+	for(int i = 0; i < NUM_PROCS; i++){
+		procs[i]->ac_sim_stats.time = sc_simulation_time();
+		procs[i]->ac_sim_stats.print();
+	}
+#else
+	mips1_proc1.ac_sim_stats.time = sc_simulation_time();
+	mips1_proc1.ac_sim_stats.print();
+#endif
 #endif
 
 #ifdef AC_DEBUG
-  ac_close_trace();
+	ac_close_trace();
 #endif
-
-  return mips_proc1.ac_exit_status;
+#ifdef NUM_PROCS
+	return proc[0]->.ac_exit_status;
+#else
+	return mips_proc1.ac_exit_status;
+#endif
 }
