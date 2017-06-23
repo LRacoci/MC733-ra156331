99bd8b54ca6a0ca38609b85d929abc26397f9674
LRacoci <racoci.0@gmail.com>
2017-06-23 11:47:05 +0000

    Antes de mudar pra soh um periférico

--- trabalho3/mips-tlm/trigonometric/trigonometric.cpp
+++ trabalho3/mips-tlm/trigonometric/trigonometric.cpp
@@ -39,8 +39,12 @@ ac_tlm_trigonometric::~ac_tlm_trigonometric() {
 ac_tlm_rsp_status ac_tlm_trigonometric::writem( const uint32_t &a , const uint32_t &d )
 {
 	//cout << "writing... addr: " <<  std::hex  << a << " data: " << d << endl;
-	uint32_t aux = ntohl(d);
-	arg = *((float*) &aux);
+	//uint32_t aux = ntohl(d);
+	//arg = *((float*) &aux);
+	uint32_t data = be32toh(d);
+	float fdata = *reinterpret_cast<float*>(&data);
+	arg = fdata;
+	cout << "Simulator arg:" <<  arg << endl;
 	return SUCCESS;
 }
 
@@ -54,18 +58,23 @@ ac_tlm_rsp_status ac_tlm_trigonometric::readm( const uint32_t &a , uint32_t &d )
 {
 	//cout << "reading... addr: " << std::hex << a << " data: " << d << endl;
 	// Calcula coseno
+	float fdata;
 	if (a == COS_ADD) {
-		arg = cos(arg);
-		uint32_t aux = *((uint32_t *) &arg);
-                d = htonl(aux);
+		//uint32_t aux = *((uint32_t *) &arg);
+        //d = htonl(aux);
+		fdata = cos(arg);
 	}
 	// Calcula seno
 	else 
 	if (a == SIN_ADD) {
-		arg = sin(arg);
-		uint32_t aux = *((uint32_t *) &arg);
-                d = htonl(aux);
+		//uint32_t aux = *((uint32_t *) &arg);
+        //d = htonl(aux);
+        fdata = sin(arg);
 	}
+    uint32_t data = htobe32(*reinterpret_cast<uint32_t*>(&fdata));
+
+    d = data;
+	
 	return SUCCESS;
 }
 
